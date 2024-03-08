d95472f3a50187be7ce5d9b2d2f67c46e09b7e89
songmeric <jws119@ic.ac.uk>
2024-03-08 14:46:27 +0000

    hi

--- client.py
+++ client.py
@@ -1,52 +1,53 @@
-import threading
 import socket
-# add a text box somewhere to initialise the username 
-alias = input('Choose an alias >>> ')
-# need to figure out how room initialisation works 
-room = 0
-# probably query the FPGA on initialisation for the value 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('', ))
-currentMessage = ""
+import threading
+import sys
 
+# Server's IP address
+# If the server is not on this machine,
+# put the private (network) or public (internet) IP address
+SERVER_HOST = '18.133.73.205'  # The server's hostname or IP address
+SERVER_PORT = 1492 
 
-def client_receive():
+# Initialize socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+try:
+    client_socket.connect((SERVER_HOST, SERVER_PORT))
+except ConnectionRefusedError:
+    print("Failed to connect to the server")
+    sys.exit()
+
+print(f"Connected to the server at {SERVER_HOST}:{SERVER_PORT}")
+
+def receive_messages():
     while True:
         try:
-            message = client.recv(1024).decode('utf-8')
-            if message == "alias?":
-                client.send(alias.encode('utf-8'))
-            elif message == "room?":
-                client.send(str(room).encode('utf-8'))
-            else:
-                print(message)
-        except:
-            print('Error!')
-            client.close()
+            message = client_socket.recv(1024).decode('utf-8')
+            print(message)
+        except Exception as e:
+            # Any error in receiving data implies the connection is closed
+            print("Disconnected from the server.")
+            client_socket.close()
             break
 
-
-def build_message():
-    while True:
-        # retreive input data 
-        currentMessage = ""
-        # send to GUI display, update label 
-
-def client_send():
-    while True:
-        message = f'{currentMessage}'
-        client.send(message.encode('utf-8'))
-
-def change_room(newRoom):
-    room = newRoom
-    client.send((f'#### room: ({room})').encode('utf-8'))
-
-
-receive_thread = threading.Thread(target=client_receive)
+# Thread for receiving messages
+receive_thread = threading.Thread(target=receive_messages)
 receive_thread.start()
 
-send_thread = threading.Thread(target=client_send)
-send_thread.start()
+try:
+    while True:
+        # Input command or message
+        message = input('')
+        # Send message to the server
+        if message:
+            client_socket.send(message.encode('utf-8'))
+        if message == '/quit':
+            break
+except KeyboardInterrupt:
+    # Handle abrupt client closure
+    print("\nExiting gracefully...")
+finally:
+    # Close socket on any exit
+    client_socket.close()
 
-display_thread = threading.Thread(target=build_message)
-display_thread.start()
